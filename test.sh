0d4da5031c7bfe54a223ac88d92b7a91dfd1b532
Bohdan Mateiko <bohdan.id27182@gmail.com>
2024-01-29 21:10:24 +0000

    test change

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 echo "INFO: Starting a MySQL database server for tests"
-docker run --name mysql -e MYSQL_ROOT_PASSWORD=P@ssw0rd -v $(pwd):/scripts -p 3307:3306 -d mysql:8.0-debian
+docker network create -d bridge test-network
+docker run --name mysql --network=test-network --hostname mysql  \
+-e MYSQL_ROOT_PASSWORD=P@ssw0rd -v $(pwd):/scripts -d mysql:8.0-debian
 
 echo "INFO: Wainting for database server to initialize"
 sleep 15 
@@ -8,10 +10,9 @@ echo "INFO: Creating a database for test"
 docker exec mysql sh -c 'mysql -u root -pP@ssw0rd < /scripts/test-queries/1-create-database.sql'
 
 echo "INFO: Starging a liquibase container"
-hostIP=$(hostname -i)
-docker run -v $(pwd):/repos --workdir /repos/ -e INSTALL_MYSQL=true \
+docker run --network=test-network -v $(pwd):/repos --workdir /repos/ -e INSTALL_MYSQL=true \
     -e LIQUIBASE_COMMAND_USERNAME=root \
     -e LIQUIBASE_COMMAND_PASSWORD=P@ssw0rd \
-    -e LIQUIBASE_COMMAND_URL=jdbc:mysql://$hostIP:3307/ShopDB \
+    -e LIQUIBASE_COMMAND_URL=jdbc:mysql://mysql:3306/ShopDB \
     liquibase/liquibase liquibase update --labels="0.0.1"
 
